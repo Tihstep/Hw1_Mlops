3c5e28185e393c21f9c0040f8c5aa8572f9f6305
Tihstep <TihstepML@gmail.com>
2024-11-10 00:35:03 +0300

    Add raise mistakes and mistake processing for fastapi scripts

--- fastapi_app/fastapi.py
+++ fastapi_app/fastapi.py
@@ -9,21 +9,30 @@ def show_models():
 
 @app.post("/train")
 def train_model_endpoint(request: TrainRequest):
-    model_type = train_model(request.model_type,
-                           request.hyperparameters,
-                           request.data
-                           )
-    return {"model_type": model_type}
+    try:
+        model_id = train_model(request.model_type,
+                               request.hyperparameters,
+                               request.data
+                               )
+        return {"model_id": model_id}
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 @app.post("/predict")
 def predict_endpoint(request: PredictRequest):
-    predictions = predict(request.model_id, request.data)
-    return {"predictions": predictions}
+    try:
+        predictions = predict(request.model_id, request.data)
+        return {"predictions": predictions}
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
 
 @app.delete("/delete")
 def delete_model_endpoint(request: : DeleteRequest):
-    pass
+    if delete_model(request.model_id):
+        return {"status": "deleted"}
+    raise HTTPException(status_code=404, detail="Sorry, model not found!")
 
 @app.get("/status")
 def health_check():
     return {"status": "All is OK! Server fill himself good!"}
+
