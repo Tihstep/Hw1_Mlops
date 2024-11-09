840aaa447b62e1c9ee19a7f8f3fb6e386a690559
Tihstep <TihstepML@gmail.com>
2024-11-10 00:26:16 +0300

    Add typing to models script and mistake catch

--- fastapi_app/model_framework.py
+++ fastapi_app/model_framework.py
@@ -1,22 +1,27 @@
 from sklearn.linear_model import LogisticRegression
+from typing import Dict, Any
 
 models = {}
 
-def train_model(model_type, hyperparameters, data):
+def train_model(model_type: str, hyperparameters: Dict[str, Any], data: list):
     if model_type == "logreg_with_regularization":
         model = LogisticRegression(**hyperparameters)
     elif model_type == "logreg_without_regularization":
         model = LogisticRegression(**hyperparameters)
+    else:
+        raise ValueError("Wrong unsupported model type")
 
-    model.fit(X, y)
+    #train_data, target = split(data)
+
+    model.fit(train_data, target)
     models[model_type] = model
     return model_type
 
-def predict(model_type, data):
+def predict(model_id: str, data: list):
     model = models.get(model_type)
     return model.predict(data).tolist()
 
-def delete_model(model_type):
+def delete_model(model_id: str):
     if model_type in models:
         del models[model_type]
         return True
@@ -25,3 +30,5 @@ def delete_model(model_type):
 def list_models():
     return ["logreg_with_regularization", 
             "logreg_without_regularization"]
+
+            
