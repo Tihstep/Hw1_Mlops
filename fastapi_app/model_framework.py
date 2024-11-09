6fcc0071773db0a27b88db15d3e9e613ace74385
Tihstep <TihstepML@gmail.com>
2024-11-10 00:20:59 +0300

    Add train, predict, delete methods

--- fastapi_app/model_framework.py
+++ fastapi_app/model_framework.py
@@ -3,13 +3,25 @@ from sklearn.linear_model import LogisticRegression
 models = {}
 
 def train_model(model_type, hyperparameters, data):
-    pass
+    if model_type == "logreg_with_regularization":
+        model = LogisticRegression(**hyperparameters)
+    elif model_type == "logreg_without_regularization":
+        model = LogisticRegression(**hyperparameters)
+
+    model.fit(X, y)
+    models[model_type] = model
+    return model_type
 
 def predict(model_type, data):
-    pass
+    model = models.get(model_type)
+    return model.predict(data).tolist()
 
 def delete_model(model_type):
-    pass
+    if model_type in models:
+        del models[model_type]
+        return True
+    return False
 
 def list_models():
-    pass
+    return ["logreg_with_regularization", 
+            "logreg_without_regularization"]
