458df090ec7bac6d611ba80b6b3a43d99c341cc7
Tihstep <TihstepML@gmail.com>
2024-11-09 23:44:12 +0300

    Add typing

--- fastapi_app/fastapi.py
+++ fastapi_app/fastapi.py
@@ -4,21 +4,21 @@ from api.pydantic import TrainRequest, PredictRequest, DeleteRequest
 app = FastAPI()
 
 @app.get("/models")
-def get_model_types():
+def show_models():
     return list_models()
 
 @app.post("/train")
-def train_model_endpoint(request):
+def train_model_endpoint(request: TrainRequest):
     model_id = train_model(request.model_type, request.hyperparameters)
     return {"model_id": model_id}
 
 @app.post("/predict")
-def predict_endpoint(request):
+def predict_endpoint(request: PredictRequest):
     predictions = predict(request.model_id, request.data)
     return {"predictions": predictions}
 
 @app.delete("/delete")
-def delete_model_endpoint(request):
+def delete_model_endpoint(request: : DeleteRequest):
     pass
 
 @app.get("/status")
