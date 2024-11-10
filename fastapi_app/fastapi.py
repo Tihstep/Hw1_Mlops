f65c7159ba44fcde36319a2e3e6ae45aea28ccc3
Tihstep <TihstepML@gmail.com>
2024-11-10 11:24:24 +0300

    Add docstring and some mistake processing

--- fastapi_app/fastapi.py
+++ fastapi_app/fastapi.py
@@ -10,6 +10,10 @@ def show_models():
 
 @app.post("/train")
 def train_model_endpoint(request: TrainRequest):
+    """
+    API trigger model training for specified model type on
+    transferred data with given hyperparametes. Return id of model.
+    """
     try:
         model_id = train_model(request.model_type,
                                request.hyperparameters,
@@ -21,6 +25,7 @@ def train_model_endpoint(request: TrainRequest):
 
 @app.post("/predict")
 def predict_endpoint(request: PredictRequest):
+    """API trigger inference for specified model type transferred data."""
     try:
         predictions = predict(request.model_id, request.data)
         return {"predictions": predictions}
@@ -29,11 +34,13 @@ def predict_endpoint(request: PredictRequest):
 
 @app.delete("/delete")
 def delete_model_endpoint(request: : DeleteRequest):
+    """API delete model from model registry(dict)"""
     if delete_model(request.model_id):
         return {"status": "deleted"}
     raise HTTPException(status_code=404, detail="Sorry, model not found!")
 
 @app.get("/status")
+"""API check if servise is available."""
 def health_check():
     return {"status": "All is OK! Server fill himself good!"}
 
--- fastapi_app/model_framework.py
+++ fastapi_app/model_framework.py
@@ -3,7 +3,25 @@ from typing import Dict, Any
 
 models = {}
 
-def train_model(model_type: str, hyperparameters: Dict[str, Any], data: list):
+
+def train_model(model_type: str, hyperparameters: Dict[str, Any], data: Dict[str, Any]):
+    """
+    Train a model and store it in the models dictionary.
+
+        Parameters
+        ----------
+        model_type : `str`
+            Which type of model will be trained.
+        hyperparameters: Dict[str, Any] : `Dict[str, Any]`
+            Human setting parameters needed to learn model.
+        data: `Dict[str, Any]`
+            Train_data and target_data for training of model.
+
+        Returns
+        -------
+        model_type : `str`
+            Name of model type
+    """
     if model_type == "logreg_with_regularization":
         model = LogisticRegression(**hyperparameters)
     elif model_type == "logreg_without_regularization":
@@ -11,23 +29,44 @@ def train_model(model_type: str, hyperparameters: Dict[str, Any], data: list):
     else:
         raise ValueError("Wrong unsupported model type")
 
-    #train_data, target = split(data)
+    target = data['target']
+    train_data = data['train_data']
+
 
     model.fit(train_data, target)
     models[model_type] = model
     return model_type
 
-def predict(model_id: str, data: list):
+def predict(model_type: str, data: list):
+    """
+    Make a prediction using a model_type model
+    
+        Parameters
+        ----------
+        model_type : `str`
+            Which type of model will be used for prediction.
+        data: `Dict[str, Any]`
+            Train_data and target_data for training of model.
+
+        Returns
+        -------
+        prediction : `list`
+            Result of model usage for data.
+    """
     model = models.get(model_type)
+    if not model:
+        raise ValueError("Model not found")
     return model.predict(data).tolist()
 
-def delete_model(model_id: str):
+def delete_model(model_type: str):
+    """Delete a model_type model."""
     if model_type in models:
         del models[model_type]
         return True
     return False
 
 def list_models():
+    """List all available models"""
     return ["logreg_with_regularization", 
             "logreg_without_regularization"]
 
