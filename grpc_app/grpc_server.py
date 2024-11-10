60aae9d35b0068ee0cdb676acb1c8b6619966cbe
Tihstep <TihstepML@gmail.com>
2024-11-10 15:47:42 +0300

    Write model service

---
+++ grpc_app/grpc_server.py
@@ -0,0 +1,28 @@
+import grpc
+from concurrent import futures
+from ..fast_api.models import train_model, predict, delete_model
+import grpc.model_service_pb2 as pb2
+import grpc.model_service_pb2_grpc as pb2_grpc
+
+class ModelService(pb2_grpc.ModelServiceServicer):
+    def TrainModel(self, request, context):
+        model_id = train_model(request.model_type, dict(request.hyperparameters, [request.data]))
+        return pb2.TrainResponse(model_id=model_id)
+
+    def Predict(self, request, context):
+        predictions = predict(request.model_id, [request.data])
+        return pb2.PredictResponse(predictions=predictions)
+
+    def DeleteModel(self, request, context):
+        success = delete_model(request.model_id)
+        return pb2.DeleteResponse(success=success)
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    pb2_grpc.add_ModelServiceServicer_to_server(ModelService(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+if __name__ == "__main__":
+    serve()
